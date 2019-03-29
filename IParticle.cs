3bd59d1ac6f8910cfe7f8eebb19ad025d78fcf18
James Vertisan <jrb@vertisan.com>
2019-03-29 13:22:04 -0400

    initial publish
    initial publish

---
+++ IParticle.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Vertisan.Nebula.Entities
+{
+    public interface IParticle
+    {
+        string ParticleId { get; }
+        long gMass { get; }
+    }
+}
---
+++ Particle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vertisan.Nebula.Entities
+{
+    public class Particle : IParticle
+    {
+        public string ParticleId { get; set; }
+        public long gMass { get; set; }
+    }
+}
